6afce3366e10c0fa1609fbb3e7730b2915c6d0da
Meriana <merianaalchami@gmail.com>
2024-01-09 13:58:46 +0100

    Paginations books et authors

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -1,18 +1,19 @@
 @extends('templates.index')
 
 @section('title')
+    Books
 @stop
 
 
 @section('content')
-<h2 class="text-2xl font-bold mb-4">Books</h2>
-@include('books._index', [
-    'books' => \App\Models\Book::orderBy('created_at', 'DESC')->limit(9)->get(),
-])
+    @php
+        $books = \App\Models\Book::orderBy('created_at', 'DESC')->paginate(9);
+    @endphp
+    <h2 class="text-2xl font-bold mb-4">Books</h2>
+    @include('books._index', ['books' => $books])
 
 
 
-<div class="flex items-center justify-between my-6">Pagination </div>
+    <div class="flex  justify-center my-10">{{$books->links()}}</div>
 
 @stop
-
