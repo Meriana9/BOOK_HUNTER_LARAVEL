504ae549bb2785c7b1bf7149811a323f66b8f667
Meriana <merianaalchami@gmail.com>
2024-01-09 10:16:24 +0100

    Contraintes globales sur 'id' et 'slug'

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -46,7 +46,14 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('web')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
+
+            Route::pattern('id', '[1-9][0-9]*'); //j'ajoute ma contrainte globale
+
+
+            Route::pattern('slug', '[a-z0-9][a-z0-9\-]*[a-z0-9\-]*'); //j'ajoute ma contrainte globale
+            parent::boot();
         });
+
     }
 
     /**
